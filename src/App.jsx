3f5a026bb24950ae651fbbb11c0c54e534369016
omerozceylan <omerozceylan35@gmail.com>
2023-05-25 04:49:45 +0300

    feat: ui ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Square from './square'
 import './App.css'
 
 function App() {
   
   return (
     <>
-       <button className="square">X</button>
+       <Square>
+        
+       </Square>
 
     </>
   )
