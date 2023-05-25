af9f5e5941ccce37ca1f891bb9178a25ffa2ab91
omerozceylan <omerozceylan35@gmail.com>
2023-05-26 00:00:09 +0300

    feat: square component is ready

--- src/square.jsx
+++ src/square.jsx
@@ -1,7 +1,22 @@
-export default function Square({value}) {
+import { useState } from "react"
+
+export default function Square() {
+    const [value, setValue] = useState(null)
+
+    function handleClick(){
+        if(value === 'X'){
+            setValue('')
+        }else{
+            setValue('X')
+        }
+    }
+
     return (
         <>
-            <button className="square">{value}</button>
+            <button 
+            className="square"
+            onClick={handleClick}
+            >{value}</button>
         </>
     )
 }
