a84c973940099fe3ea39c3d1c9e4d6f856a94cd6
omerozceylan <omerozceylan35@gmail.com>
2023-05-26 22:34:58 +0300

    feat: started to write calculate winner func.

--- src/Board.jsx
+++ src/Board.jsx
@@ -3,9 +3,23 @@ import Square from "./square"
 
 export default function Board() {
     const [xIsNext, setXIsNext] = useState(true)
-
     const [squares, setSquares] = useState(Array(9).fill(null))
     
+    function calculateWinner(squares) {
+        const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6]
+        ];
+    
+        
+    }
+
     function handleClick(index){
         const nextSquares = squares.slice()
         if(nextSquares[index])return
