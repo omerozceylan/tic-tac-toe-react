9c8940d9617ac38488d2ee3dac89ce1ef3c9c727
omerozceylan <omerozceylan35@gmail.com>
2023-05-26 04:19:52 +0300

    the states are currntly stored in board component

--- src/Board.jsx
+++ src/Board.jsx
@@ -4,9 +4,9 @@ import Square from "./square"
 export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null))
     
-    function handleClick(){
+    function handleClick(index){
         const nextSquares = squares.slice()
-        nextSquares[0] = 'X'
+        nextSquares[index] = 'X'
         setSquares(nextSquares)
     }
 
@@ -17,19 +17,19 @@ export default function Board() {
     return (
         <>
             <div className="board-row">
-                <Square value={squares[0]} onSquareClick={handleClick}></Square>
-                <Square value={squares[1]} onSquareClick={handleClick}></Square>
-                <Square value={squares[2]} onSquareClick={handleClick}></Square> 
+                <Square value={squares[0]} onSquareClick={()=> handleClick(0)}></Square>
+                <Square value={squares[1]} onSquareClick={()=> handleClick(1)}></Square>
+                <Square value={squares[2]} onSquareClick={()=> handleClick(2)}></Square> 
             </div>
             <div className="board-row">
-                <Square value={squares[3]}onSquareClick={handleClick}></Square>
-                <Square value={squares[4]}onSquareClick={handleClick}></Square>
-                <Square value={squares[5]}onSquareClick={handleClick}></Square>
+                <Square value={squares[3]}onSquareClick={()=> handleClick(3)}></Square>
+                <Square value={squares[4]}onSquareClick={()=> handleClick(4)}></Square>
+                <Square value={squares[5]}onSquareClick={()=> handleClick(5)}></Square>
             </div>
             <div className="board-row">
-                <Square value={squares[6]}onSquareClick={handleClick}></Square>
-                <Square value={squares[7]}onSquareClick={handleClick}></Square>
-                <Square value={squares[8]}onSquareClick={handleClick}></Square>
+                <Square value={squares[6]}onSquareClick={()=> handleClick(6)}></Square>
+                <Square value={squares[7]}onSquareClick={()=> handleClick(7)}></Square>
+                <Square value={squares[8]}onSquareClick={()=> handleClick(8)}></Square>
             </div>
             
         </>
